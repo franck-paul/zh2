7e9592ad2095b7182e64c9ca9720009773aad473
franck <carnet.franck.paul@gmail.com>
2018-03-09 13:11:30 +0100

    Various code formatting, no more dublin core

--- _define.php
+++ _define.php
@@ -1,24 +1,22 @@
 <?php
-# -- BEGIN LICENSE BLOCK ----------------------------------
-# This file is part of zh2, a theme for Dotclear 2.
-# based on WP eponym theme from Leo Babuta (http://leobabauta.com and http://zenhabits.net/theme)
-#
-# Copyright (c) Franck Paul and contributors
-# carnet.franck.paul@gmail.com
-#
-# Licensed under the GPL version 2.0 license.
-# A copy of this license is available in LICENSE file or at
-# http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
-# -- END LICENSE BLOCK ------------------------------------
+/**
+ * @brief zh2, a theme for Dotclear 2
+ *
+ * @package Dotclear
+ * @subpackage Themes
+ *
+ * @copyright Olivier Meunier & Association Dotclear
+ * @copyright GPL-2.0-only
+ */
 
-if (!defined('DC_RC_PATH')) { return; }
+if (!defined('DC_RC_PATH')) {return;}
 
 $this->registerModule(
-	/* Name */			"zh2",
-	/* Description*/	"Zen habits (based on eponym Wordpress theme designed by Leo Babuta)",
-	/* Author */		"Franck Paul",
-	/* Version */		'1.3',
-	array(
-		/* Type */		'type' =>			'theme'
-	)
+    "zh2",                                                                 // Name
+    "Zen habits (based on eponym Wordpress theme designed by Leo Babuta)", // Description
+    "Franck Paul",                                                         // Author
+    '1.4',                                                                 // Version
+    array(
+        'type' => 'theme' // Type
+    )
 );
--- _public.php
+++ _public.php
@@ -1,39 +1,50 @@
 <?php
-# -- BEGIN LICENSE BLOCK ----------------------------------
-# This file is part of zh2, a theme for Dotclear 2.
-# based on WP eponym theme from Leo Babuta (http://leobabauta.com and http://zenhabits.net/themes)
-#
-# Copyright (c) Franck Paul and contributors
-# carnet.franck.paul@gmail.com
-#
-# Licensed under the GPL version 2.0 license.
-# A copy of this license is available in LICENSE file or at
-# http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
-# -- END LICENSE BLOCK ------------------------------------
+/**
+ * @brief zh2, a theme for Dotclear 2
+ *
+ * @package Dotclear
+ * @subpackage Themes
+ *
+ * @copyright Olivier Meunier & Association Dotclear
+ * @copyright GPL-2.0-only
+ */
 
-if (!defined('DC_RC_PATH')) { return; }
+namespace themes\zh2;
 
-l10n::set(dirname(__FILE__).'/locales/'.$_lang.'/main');
+if (!defined('DC_RC_PATH')) {return;}
+
+\l10n::set(dirname(__FILE__) . '/locales/' . $_lang . '/main');
 
 # Templates
-$core->tpl->addBlock('IfPreviewIsNotMandatory',array('tplZh2Theme','IfPreviewIsNotMandatory'));
+$core->tpl->addBlock('IfPreviewIsNotMandatory', array(__NAMESPACE__ . '\tplZh2Theme', 'IfPreviewIsNotMandatory'));
+
+# Behaviors
+$core->addBehavior('tplIfConditions', array(__NAMESPACE__ . '\tplZh2Theme', 'tplIfConditions'));
 
 class tplZh2Theme
 {
-	public static function IfPreviewIsNotMandatory($attr,$content)
-	{
-		$theme_ident = preg_replace('/[^a-zA-Z0-9_]/','_',$GLOBALS['core']->blog->settings->system->theme).'_style';
-		$s = $GLOBALS['core']->blog->settings->themes->get($theme_ident);
-		if ($s !== null) {
-			$s = @unserialize($s);
-			if (is_array($s)) {
-				if (isset($s['preview_not_mandatory'])) {
-					if ($s['preview_not_mandatory']) {
-						return $content;
-					}
-				}
-			}
-		}
-		return '';
-	}
+    public static function tplIfConditions($tag, $attr, $content, $if)
+    {
+        if ($tag == 'SysIf' && isset($attr['has_blog_descr'])) {
+            $sign = (boolean) $attr['has_blog_descr'] ? '' : '!';
+            $if[] = $sign . '(strlen($core->blog->desc) !== 0)';
+        }
+    }
+
+    public static function IfPreviewIsNotMandatory($attr, $content)
+    {
+        $theme_ident = preg_replace('/[^a-zA-Z0-9_]/', '_', $GLOBALS['core']->blog->settings->system->theme) . '_style';
+        $s           = $GLOBALS['core']->blog->settings->themes->get($theme_ident);
+        if ($s !== null) {
+            $s = @unserialize($s);
+            if (is_array($s)) {
+                if (isset($s['preview_not_mandatory'])) {
+                    if ($s['preview_not_mandatory']) {
+                        return $content;
+                    }
+                }
+            }
+        }
+        return '';
+    }
 }
