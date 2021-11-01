104818d6b77510375f104927ec8960e9a7861bfa
franck <carnet.franck.paul@gmail.com>
2021-11-01 09:29:03 +0100

    Update default branch in _define.php for alternative repository

--- _define.php
+++ _define.php
@@ -23,6 +23,6 @@ $this->registerModule(
 
         'details'    => 'https://open-time.net/?q=zh2',       // Details URL
         'support'    => 'https://github.com/franck-paul/zh2', // Support URL
-        'repository' => 'https://raw.githubusercontent.com/franck-paul/zh2/main/dcstore.xml'
+        'repository' => 'https://raw.githubusercontent.com/franck-paul/zh2/master/dcstore.xml'
     ]
 );
